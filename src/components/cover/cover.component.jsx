435e1d070a8507f80b3fdcb5e03587028a99a07e
folka2134 <folka2134@gmail.com>
2021-11-20 10:14:22 +0000

    Added disable/enable scroll state

--- src/components/cover/cover.component.jsx
+++ src/components/cover/cover.component.jsx
@@ -1,16 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
+import disableScroll from 'disable-scroll';
 
 import { Link } from 'react-scroll';
 
 
 export const Cover = () => {
+    const [scroll, setScroll] = useState(disableScroll.on())
+    const startScroll = () => {
+        setScroll(disableScroll.off())
+    }
+    
     return (
         <div  className="cover-container title h-screen w-full flex justify-center items-center bg-black">
-            
             <Link to="banner-container" smooth={true}>
                 <div className="title text-orange-default text-5xl cursor-pointer">
                     {/* <h1 className="">Staff Party</h1> */}
-                    <div className="overlay rounded-2xl">Staff Party</div>
+                    <div className="overlay rounded-2xl" onClick={startScroll}>Staff Party</div>
                 </div>
             </Link>
         </div>
